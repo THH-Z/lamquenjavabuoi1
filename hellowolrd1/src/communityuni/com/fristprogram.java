d4afaab34e896492d1bc25c25398f05fc1dcae59
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-18 10:54:49 +0700

    thuchanhbuoi2

---
+++ hellowolrd1/src/communityuni/com/fristprogram.java
@@ -0,0 +1,10 @@
+package communityuni.com;
+
+public class fristprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("xin chào bạn");
+	}
+
+}
---
+++ hellowolrd1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module hellowolrd1 {
+}
