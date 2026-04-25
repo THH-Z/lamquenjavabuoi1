95df4712625aec596e554f24714ef3bc4e949f23
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-25 09:37:48 +0700

    bai tap 4  ngay 25 / 4

---
+++ thuchanhbuoi254/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module thuchanhbuoi254 {
+}
---
+++ thuchanhbuoi254/src/thuchanhbuoi254/bai4thuchanh254.java
@@ -0,0 +1,10 @@
+package thuchanhbuoi254;
+
+public class bai4thuchanh254 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
