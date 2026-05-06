1ee196686f2efea6421708209a3bb9b943dc77d4
THH-Z <truonghoanghuy27032006@gmail.com>
2026-05-06 12:01:17 +0700

    quanlynhanvien
    oop

---
+++ thachhanhoopjava/src/thachhanhoopjava/quanlynhanvien.java
@@ -0,0 +1,38 @@
+package thuchanhoopjava;
+import java.util.*;
+public class quanlynhanvien {
+	
+	
+	
+public class NhanVien
+{
+	private String ho,ten;
+	private  int soSP;
+	
+	public NhanVien(String string, String string2, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setho(String ho) { this.ho=ho;}
+	
+	public void seten(String ten) { this.ten=ten;}
+	
+	public void setsoSp(int soSP) { this.soSP=soSP;}
+	
+	
+	public String getho(String ho) { return this.ho;}
+	
+	public String getten(String ten) { return this.ten;}
+	
+	public int getsoSp(int soSP) { return this.soSP;}
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		NhanVien nv = nv.new NhanVien("truong","huy",7);
+			
+	}
+
+}
